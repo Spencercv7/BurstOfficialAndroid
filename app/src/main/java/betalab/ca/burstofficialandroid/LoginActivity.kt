4dd0ac5c83d9b1a848781133427758f761f01eb7
Bruce Nishimura <bruceninga@gmail.com>
2019-03-18 15:03:47 -0400

    Added interests screen

--- app/src/main/java/betalab/ca/burstofficialandroid/LoginActivity.kt
+++ app/src/main/java/betalab/ca/burstofficialandroid/LoginActivity.kt
@@ -4,15 +4,20 @@ import android.os.Bundle
 import android.view.inputmethod.EditorInfo
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_calendar.*
 import kotlinx.android.synthetic.main.activity_landing.*
 import kotlinx.android.synthetic.main.activity_login.*
+//import kotlinx.android.synthetic.main.activity_notifications.*
 import kotlinx.android.synthetic.main.activity_onboarding.*
-import kotlinx.android.synthetic.main.activity_school_an.*
 
 class LoginActivity : AppCompatActivity() {
     private val landingNumber = 0
     private val loginNumber = 1
     private val schoolNumber = 2
+    private val profileNumber = 3
+    private val importNumber = 4
+    private val notificationNumber = 5
+    private val interestsNumber = 6
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_onboarding)
@@ -34,7 +39,20 @@ class LoginActivity : AppCompatActivity() {
         back_button_login.setOnClickListener { back() }
 
         //activity_school_an screen
-        button_back.setOnClickListener { back() }
+//        button_back.setOnClickListener { back() }
+//        button_profile.setOnClickListener { profile() } //temp will change later
+//
+//        profile_back.setOnClickListener { register() }
+//        profile_next_calendar.setOnClickListener { calendar() }
+
+        //activity_calendar screen
+        calendar_back.setOnClickListener { back() }
+        calendar_skip.setOnClickListener { notifications() }
+
+        //notification_skip.setOnClickListener { interests() }
+        //notifications_back.setOnClickListener { calendar() }
+
+
     }
     private fun back() {
         viewFlipper.displayedChild = landingNumber
@@ -47,6 +65,18 @@ class LoginActivity : AppCompatActivity() {
     }
     private fun launchLoginScreen() {
         viewFlipper.displayedChild = loginNumber
+    }
+    private fun profile() {
+        viewFlipper.displayedChild = profileNumber
+    }
+    private fun calendar() {
+        viewFlipper.displayedChild = importNumber
+    }
+    private fun notifications() {
+        viewFlipper.displayedChild = notificationNumber
+    }
+    private fun interests() {
+        viewFlipper.displayedChild = interestsNumber
     }
 
 
