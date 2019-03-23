cd7e2129d188c597879c9ae3e1d258aaff890340
Ben Lammers <16bdl2@queensu.ca>
2019-03-23 19:55:17 -0400

    Added Explore Fragment and Explore Adapter

---
+++ app/src/main/java/betalab/ca/burstofficialandroid/ExploreFragment.kt
@@ -0,0 +1,33 @@
+package betalab.ca.burstofficialandroid
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class ExploreFragment : Fragment() {
+
+    private lateinit var recyclerView: RecyclerView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.explore_fragment,
+                container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        recyclerView = view.findViewById(R.id.explore_fragment_recycler_view) as RecyclerView
+        recyclerView.layoutManager = LinearLayoutManager(activity, LinearLayoutManager.HORIZONTAL, false)
+        recyclerView.adapter = ExploreAdapter()
+        recyclerView.hasFixedSize()
+    }
+
+}
