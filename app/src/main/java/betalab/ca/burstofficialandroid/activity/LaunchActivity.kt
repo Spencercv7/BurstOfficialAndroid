50cfc7c0ef07977f834ecf995fe1c43356b5c53e
Ben Lammers <16bdl2@queensu.ca>
2019-03-29 01:19:01 -0400

    Implemented Drawer Navigation Menu Reorganized app navigation flow

--- app/src/main/java/betalab/ca/burstofficialandroid/activity/LaunchActivity.kt
+++ app/src/main/java/betalab/ca/burstofficialandroid/activity/LaunchActivity.kt
@@ -10,7 +10,7 @@ class LaunchActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val intent = Intent(this, MainActivity::class.java).apply {
+        val intent = Intent(this, LoginActivity::class.java).apply {
             addFlags(FLAG_ACTIVITY_NEW_TASK)
         }
         startActivity(intent)
