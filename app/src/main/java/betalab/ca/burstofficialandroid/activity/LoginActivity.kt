36534dccbfefdc6470ea161aaec22a745b928ae1
Bruce Nishimura <bruceninga@gmail.com>
2019-04-01 13:49:07 -0400

    Cleaned up loginactivity and fixed height on one of the recyclerviews

--- app/src/main/java/betalab/ca/burstofficialandroid/activity/LoginActivity.kt
+++ app/src/main/java/betalab/ca/burstofficialandroid/activity/LoginActivity.kt
@@ -7,6 +7,7 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import betalab.ca.burstofficialandroid.R
 import kotlinx.android.synthetic.main.activity_calendar.*
+import kotlinx.android.synthetic.main.activity_interests.*
 import kotlinx.android.synthetic.main.activity_landing.*
 import kotlinx.android.synthetic.main.activity_login.*
 import kotlinx.android.synthetic.main.activity_onboarding.*
@@ -15,17 +16,16 @@ import kotlinx.android.synthetic.main.notif_an.*
 import kotlinx.android.synthetic.main.profile_picture_an.*
 
 class LoginActivity : AppCompatActivity() {
-    private val landingNumber = 0
-    private val loginNumber = 1
-    private val schoolNumber = 2
-    private val profileNumber = 3
-    private val importNumber = 4
-    private val notificationNumber = 5
-    private val interestsNumber = 6
+    enum class SCREEN(val value:Int) {
+        LANDING(0), LOGIN(1),
+        SCHOOL(2), PROFILE(3),
+        IMPORT(4), NOTIFICATION(5),
+        INTERESTS(6);
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_onboarding)
-        viewFlipper.displayedChild = landingNumber
+        setScreen(SCREEN.LANDING)
         password_edit_text.editText?.setOnEditorActionListener { _, actionId, _ ->
             if (actionId == EditorInfo.IME_ACTION_GO)
                 loginToApp()
@@ -33,60 +33,45 @@ class LoginActivity : AppCompatActivity() {
         }
         instantiateListeners()
     }
+    private fun setScreen(screen: SCREEN) {
+        viewFlipper.displayedChild = screen.value
+    }
     private fun instantiateListeners() {
         //landing screen
-        get_started_button.setOnClickListener { register() }
-        login_button.setOnClickListener { launchLoginScreen() }
+        get_started_button.setOnClickListener { setScreen(SCREEN.SCHOOL) }
+        login_button.setOnClickListener { setScreen(SCREEN.LOGIN) }
 
         //Login screen
         login_login_button.setOnClickListener { loginToApp() }  //open main activity
-        back_button_login.setOnClickListener { back() } //back to landing
+        back_button_login.setOnClickListener { setScreen(SCREEN.LANDING) } //back to landing
 
         //activity_school_an screen
-        button_back.setOnClickListener { back() }   //back to landing
-        button_profile.setOnClickListener { profile() } //temp will change later
+        button_back.setOnClickListener { setScreen(SCREEN.LANDING) }   //back to landing
+        button_profile.setOnClickListener { setScreen(SCREEN.PROFILE) } //temp will change later
 
-        skip_button_notifications.setOnClickListener { launchLoginScreen()
-        Toast.makeText(this, "Should go to interests but skips for now", Toast.LENGTH_LONG).show()}
-        //ABOVE should go to interests but for now skip after "creating account" and goes to login so you
-        //login with newly created account
-        location_button_notifications.setOnClickListener { calendar() }
+        //profile screen
+        calendar_button_profile.setOnClickListener { setScreen(SCREEN.IMPORT) }
+        location_button_profile.setOnClickListener { setScreen(SCREEN.SCHOOL) }
 
-        calendar_button_profile.setOnClickListener { calendar() }
-        location_button_profile.setOnClickListener { register() }
+        //
+        calendar_back.setOnClickListener { setScreen(SCREEN.PROFILE) } //Go back to profile
+        calendar_skip.setOnClickListener { setScreen(SCREEN.NOTIFICATION) } //forward to notification screen
 
-        calendar_back.setOnClickListener { profile() }
-        calendar_skip.setOnClickListener { notifications() }
+        //notification screen
+        skip_button_notifications.setOnClickListener { setScreen(SCREEN.INTERESTS) }
+        location_button_notifications.setOnClickListener { setScreen(SCREEN.IMPORT) }
 
+        //Interests screen
+        confirm_interests_button.setOnClickListener { loginToApp() }
 
     }
-    private fun back() {
-        viewFlipper.displayedChild = landingNumber
-    }
+
     private fun loginToApp() {
         val intent = Intent(this, MainActivity::class.java).apply {
             addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
         }
         startActivity(intent)
         finish()
-    }
-    private fun register() {
-        viewFlipper.displayedChild = schoolNumber
-    }
-    private fun launchLoginScreen() {
-        viewFlipper.displayedChild = loginNumber
-    }
-    private fun profile() {
-        viewFlipper.displayedChild = profileNumber
-    }
-    private fun calendar() {
-        viewFlipper.displayedChild = importNumber
-    }
-    private fun notifications() {
-        viewFlipper.displayedChild = notificationNumber
-    }
-    private fun interests() {
-        viewFlipper.displayedChild = interestsNumber
     }
 
 
