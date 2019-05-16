7415ddacdc909bd4d42148b84c0e759d921f2142
benlammers <21benlammers@gmail.com>
2019-05-15 22:25:32 -0400

    Accessing url for ics schedule file still need to use url to download ics file

--- app/src/main/java/betalab/ca/burstofficialandroid/ui/activity/LoginActivity.kt
+++ app/src/main/java/betalab/ca/burstofficialandroid/ui/activity/LoginActivity.kt
@@ -1,16 +1,28 @@
 package betalab.ca.burstofficialandroid.ui.activity
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
+import android.graphics.Bitmap
+import android.media.Image
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Log
 import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
+import android.webkit.JavascriptInterface
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.ImageView
+import android.widget.Toast
+import android.widget.ViewFlipper
 import androidx.appcompat.app.AppCompatActivity
 import betalab.ca.burstofficialandroid.R
 import betalab.ca.burstofficialandroid.ui.util.ValidationUtils
 import kotlinx.android.synthetic.main.activity_onboarding.*
+import kotlinx.android.synthetic.main.fragment_calendar.*
+import kotlinx.android.synthetic.main.import_class_calendar.*
 import kotlinx.android.synthetic.main.onboarding_calendar.*
 import kotlinx.android.synthetic.main.onboarding_interests.*
 import kotlinx.android.synthetic.main.onboarding_landing.*
@@ -18,6 +30,10 @@ import kotlinx.android.synthetic.main.onboarding_login.*
 import kotlinx.android.synthetic.main.onboarding_notifications.*
 import kotlinx.android.synthetic.main.onboarding_profile.*
 import kotlinx.android.synthetic.main.onboarding_school.*
+import android.webkit.JsResult
+import android.webkit.WebChromeClient
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
 
 
 class LoginActivity : AppCompatActivity() {
@@ -25,13 +41,14 @@ class LoginActivity : AppCompatActivity() {
         LANDING(0), LOGIN(1),
         SCHOOL(2), PROFILE(3),
         IMPORT(4), NOTIFICATION(5),
-        INTERESTS(6);
+        INTERESTS(6), IMPORT_CLASS_CALENDAR(7);
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_onboarding)
-        setScreen(SCREEN.LANDING)
+        Toast.makeText(this, "Starts at import screen for testing purposes", Toast.LENGTH_LONG).show()
+        setScreen(SCREEN.IMPORT) //TODO: SWITCH BACK TO LANDING
         password_edit_text.editText?.setOnEditorActionListener { _, actionId, _ ->
             if (actionId == EditorInfo.IME_ACTION_GO)
                 loginToApp()
@@ -121,6 +138,8 @@ class LoginActivity : AppCompatActivity() {
         //Calendar screen
         calendar_back.setOnClickListener { setScreen(SCREEN.PROFILE) } //Go back to profile
         calendar_skip.setOnClickListener { setScreen(SCREEN.NOTIFICATION) } //forward to notification screen
+        import_class_calendar_button.setOnClickListener { importClassCalendar()}
+        import_class_back_arrow.setOnClickListener { setScreen(SCREEN.IMPORT) }
 
         //notification screen
         skip_button_notifications.setOnClickListener { setScreen(SCREEN.INTERESTS) }
@@ -144,6 +163,46 @@ class LoginActivity : AppCompatActivity() {
         email_register.error = ValidationUtils.isEmailValid(email_register.editText?.text.toString())
         password_register.error = ValidationUtils.isPasswordValid(password_register.editText?.text.toString())
         return name_register.error == null && email_register.error == null && password_register.error == null
+    }
+
+
+
+
+    //IMPORT CLASS CALENDAR HANDLING
+    @SuppressLint("SetJavaScriptEnabled")
+    private fun importClassCalendar() {
+        setScreen(SCREEN.IMPORT_CLASS_CALENDAR)
+        import_class_webview.settings.javaScriptEnabled = true
+        import_class_webview.addJavascriptInterface(MyJavaScriptInterface(this), "HtmlViewer")
+
+        import_class_webview.webViewClient = object : WebViewClient() {
+            override fun onPageFinished(view: WebView, url: String) {
+                import_class_webview.loadUrl("javascript:HtmlViewer.showHTML" +     //read the html of the page
+                        "('<html>'+document.getElementsByTagName('html')[0].innerHTML+'</html>');")
+
+                if (url == "https://my.queensu.ca/software-centre" ) {   //If the webview gets to the software page exit the screen
+                    setScreen(SCREEN.IMPORT)
+                }
+            }
+        }
+        import_class_webview.loadUrl("https://my.queensu.ca/software-centre")
+    }
+
+
+
+    class MyJavaScriptInterface(private val ctx: Context) {
+
+        @JavascriptInterface
+        fun showHTML(html: String) {
+            if (html.contains("https://mytimetable.queensu.ca/timetable")) {    //stop if html contains this url
+                val icsURL = html.substring(
+                    html.indexOf("https://mytimetable.queensu.ca/timetable"),  //substring starts here
+                    html.indexOf(".</p>\n" + "  <p>Visit the ITS")     //ends right before this
+                )
+                Log.e("ICS URL", icsURL)  //TODO: FOR THE LOVE OF GOODNESS LET ME TAKE THIS VALUE SOMEHOW
+                Toast.makeText(ctx, icsURL, Toast.LENGTH_LONG).show()
+            }
+        }
     }
 }
 
