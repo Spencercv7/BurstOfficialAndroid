8fccb9669e4c234b5758b20c21be43dbf6445a72
spenc <16scpv@queensu.ca>
2019-06-03 18:05:34 -0400

    Added Catalog onClickListener and fixed data class for catalog cards and created adapter.

---
+++ app/src/main/java/betalab/ca/burstofficialandroid/model/CatalogItem.kt
@@ -0,0 +1,9 @@
+package betalab.ca.burstofficialandroid.model
+
+import android.graphics.drawable.VectorDrawable
+
+data class CatalogItem (
+    val title : String? = null,
+    val desc : String? = null,
+    val vec : VectorDrawable? = null
+)
